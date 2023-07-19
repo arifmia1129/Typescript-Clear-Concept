65143d19588c4b0a2e5952c08883e7768d15aa1a
arifmia1129 <arifmia1129@gmail.com>
2023-07-19 11:28:44 +0600

    mapped

--- src/conditional.ts
+++ src/conditional.ts
@@ -1,11 +1,11 @@
-type A = number;
-type B = string;
-type C = null;
+// type A = number;
+// type B = string;
+// type C = null;
 
-type D = A extends string
-  ? string
-  : B extends number
-  ? number
-  : C extends null
-  ? null
-  : unknown;
+// type D = A extends string
+//   ? string
+//   : B extends number
+//   ? number
+//   : C extends null
+//   ? null
+//   : unknown;
---
+++ src/mapped.ts
@@ -0,0 +1,21 @@
+const myNumbers: number[] = [1, 2, 3, 4, 5];
+
+const myString: string[] = myNumbers.map((num) => num.toString());
+
+console.log(myString);
+
+type MyFriends = {
+  friend1: string;
+  friend2: string;
+  friend3: string;
+};
+
+type FixedMyFriends = {
+  readonly [key in keyof MyFriends]: MyFriends[key];
+};
+
+const myBestFriends: FixedMyFriends = {
+  friend1: 'Binu',
+  friend2: 'Arif',
+  friend3: 'Shahin',
+};
