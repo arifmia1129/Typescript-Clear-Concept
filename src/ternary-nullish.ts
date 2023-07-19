4314e8cdbd2b00775f7ebc51252e3be1337fe698
arifmia1129 <arifmia1129@gmail.com>
2023-07-19 10:01:21 +0600

    ternary and nullish

---
+++ src/ternary-nullish.ts
@@ -0,0 +1,13 @@
+// ternary
+let balance: number = 12000;
+
+const checkAvailability =
+  balance > 10000 ? 'Buy new furniture' : "No, It's my out of budget";
+
+console.log(checkAvailability);
+
+// nullish
+
+const user: unknown = { name: 'arif' };
+
+console.log(user ?? 'Nothing found about user');
