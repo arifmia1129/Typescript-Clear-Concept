2cd3349b1ec60b40c25d31bc4fcba581c460453f
arifmia1129 <arifmia1129@gmail.com>
2023-07-19 06:56:01 +0600

    array and tuple

---
+++ src/primitive-type.ts
@@ -0,0 +1,15 @@
+/**
+ * string
+ * number
+ * boolean
+ * null
+ * undefined
+ * */
+
+const str: string = 'My name is Arif';
+
+const age: number = 12;
+
+const isAdult: boolean = true;
+
+const otherInfo: null = null;
