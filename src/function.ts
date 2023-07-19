9f2d21dfa5635b0e8e9d0e91bc2c87b7646ee3ef
arifmia1129 <arifmia1129@gmail.com>
2023-07-19 07:37:12 +0600

    spread rest

--- src/function.ts
+++ src/function.ts
@@ -1,16 +1,16 @@
-// normal function
+// // normal function
 
-function addWithNormalFunction(num1: number, num2: number): string {
-  const sum = num1 + num2;
+// function addWithNormalFunction(num1: number, num2: number): string {
+//   const sum = num1 + num2;
 
-  return `Total sum of ${num1} and ${num2} is: ${sum}`;
-}
+//   return `Total sum of ${num1} and ${num2} is: ${sum}`;
+// }
 
-console.log(addWithNormalFunction(2, 2));
+// console.log(addWithNormalFunction(2, 2));
 
-// arrow function
+// // arrow function
 
-const addWithArrowFunction = (num1: number, num2: number): number =>
-  num1 + num2;
+// const addWithArrowFunction = (num1: number, num2: number): number =>
+//   num1 + num2;
 
-console.log(addWithArrowFunction(3, 3));
+// console.log(addWithArrowFunction(3, 3));
---
+++ src/spread-rest.ts
@@ -0,0 +1,32 @@
+// spread
+
+const adultMember: string[] = ['Arif', 'Binu'];
+const childMember: string[] = ['Ariba', 'Abdullah'];
+
+adultMember.push(...childMember);
+
+console.log(adultMember);
+
+// rest parameter
+
+const auth = (...role: string[]): string => {
+  if (role.includes('admin')) {
+    return 'You are a valid user';
+  } else {
+    return 'Status code 401';
+  }
+};
+
+console.log(auth('admin'));
+
+const totalCost = (...cost: number[]): number => {
+  let sum = 0;
+
+  cost.map((amount: number) => {
+    sum = sum + amount;
+  });
+
+  return sum;
+};
+
+console.log(totalCost(1, 2, 3));
