7b67700baf50e7f71dc449f3471b623001b3ab78
arifmia1129 <arifmia1129@gmail.com>
2023-07-19 07:46:29 +0600

    array and object destructuring

---
+++ src/destructuring.ts
@@ -0,0 +1,28 @@
+// array
+// const familyMembers: string[] = [
+//   'Zaidul',
+//   'Nurzahan',
+//   'Arif',
+//   'Binu',
+//   'Ariba',
+//   'Abdullah',
+// ];
+
+// const [grandFather, grandMother, father, mother, child1, child2] =
+//   familyMembers;
+
+// console.log(father, child1);
+
+// object
+// const family = {
+//   grandFather: 'Zaidul',
+//   grandMother: 'Nurzahan',
+//   father: 'Arif',
+//   mother: 'Binu',
+//   child1: 'Ariba',
+//   child2: 'Abdullah',
+// };
+
+// const { mother, child2 } = family;
+
+// console.log(mother, child2);
