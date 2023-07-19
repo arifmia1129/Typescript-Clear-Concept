6f8df6e9e6c6bcb83fd505aa2a6218107ef0bb16
arifmia1129 <arifmia1129@gmail.com>
2023-07-19 20:39:25 +0600

    polymorphism

---
+++ src/polymorphism.ts
@@ -0,0 +1,23 @@
+class Father {
+  showInfo(): void {
+    console.log('This is father class');
+  }
+}
+
+class Son1 extends Father {
+  showInfo(): void {
+    console.log('This is son1 class');
+  }
+}
+
+class Son2 extends Father {
+  showInfo(): void {
+    console.log('This is son2 class');
+  }
+}
+
+function show(param: Father) {
+  param.showInfo();
+}
+
+show(new Son2());
